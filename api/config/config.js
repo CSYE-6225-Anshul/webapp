3f2469f0c7475feb59cbbdc31e7932dbb997055d
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-10-04 04:08:52 -0400

    changed config

--- api/config/config.js
+++ api/config/config.js
@@ -4,7 +4,8 @@ module.exports = {
   "development": {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "dialect": "mysql"
+    "database": process.env.MYSQL_DATABASE, 
+    "host": '127.0.0.1',
+    "dialect": 'mysql',
   }
 }
