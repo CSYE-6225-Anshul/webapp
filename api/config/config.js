6c12b1926f9595154ea12212ee256eb514267879
Anshul Sharma <sharma.ansh@northeastern.edu>
2023-10-04 03:57:52 -0400

    changed config

--- api/config/config.js
+++ api/config/config.js
@@ -2,24 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.devUsername,
-    "password": process.env.devPassword,
-    "database": process.env.devDB,
-    "host": process.env.devHost,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": process.env.testUsername,
-    "password": process.env.testPassword,
-    "database": process.env.testDB,
-    "host": process.env.testHost,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": process.env.prodUsername,
-    "password": process.env.prodPassword,
-    "database": process.env.prodDB,
-    "host": process.env.prodHost,
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
     "dialect": "mysql"
   }
 }
